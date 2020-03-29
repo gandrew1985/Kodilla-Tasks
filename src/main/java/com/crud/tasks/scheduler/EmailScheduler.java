b7007eccc60b009ec431da47bb6611aadf5f5253
gandrew1985 <gandrew@op.pl>
2020-03-29 14:07:37 +0100

    zad 19.4

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -22,11 +22,16 @@ public class EmailScheduler {
     @Autowired
     private AdminConfig adminConfig;
 
-    @Scheduled(cron = "0 0 10 * * *")
+    @Scheduled(fixedDelay = 10000)
     public void sendInformationEmail() {
 
         long size = taskRepository.count();
-        simpleEmailService.send(new Mail(adminConfig.getAdminMail(), SUBJECT,
-                "Currently in database you got: " + size + " tasks", ""));
+        if (size != 1) {
+            simpleEmailService.send(new Mail(adminConfig.getAdminMail(), SUBJECT,
+                    "Currently in database you got: " + size + " tasks", null));
+        } else {
+            simpleEmailService.send(new Mail(adminConfig.getAdminMail(), SUBJECT,
+                    "Currently in database you got: " + size + " task", null));
+        }
     }
 }
