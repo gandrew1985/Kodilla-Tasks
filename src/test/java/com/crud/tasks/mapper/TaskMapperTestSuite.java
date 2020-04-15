8e5d4ef9e9faf67c0a21aa9680bc68e115a04ddf
gandrew1985 <gandrew@op.pl>
2020-04-15 18:47:16 +0100

    zad 21.1 corr

--- src/test/java/com/crud/tasks/mapper/TaskMapperTestSuite.java
+++ src/test/java/com/crud/tasks/mapper/TaskMapperTestSuite.java
@@ -1,17 +1,53 @@
 package com.crud.tasks.mapper;
 
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class TaskMapperTestSuite {
 
+    @Autowired
+    private TaskMapper taskMapper;
 
+    @Test
+    public void testMapToTask() {
+        //Given
+        TaskDto taskDto = new TaskDto(23L, "my_task", "taskToDo");
+        //When
+        Task task = taskMapper.mapToTask(taskDto);
+        //Then
+        assertEquals(task.getId(), taskDto.getId());
+    }
 
+    @Test
+    public void testMapToTaskDto() {
+        //Given
+        Task task = new Task(23L, "my_task", "taskToDo");
+        //When
+        TaskDto taskDto = taskMapper.mapToTaskDto(task);
+        //Then
+        assertEquals(task.getId(), taskDto.getId());
+    }
+
+    @Test
+    public void testMapToTaskDtoList() {
+        //Given
+        List<Task> tasks = new ArrayList<>();
+        tasks.add(new Task(21L, "my_task", "TaskToDo"));
+        //When
+        List<TaskDto> taskDtos = taskMapper.mapToTaskDtoList(tasks);
+        //Then
+        assertEquals(1, taskDtos.size());
+    }
 }
